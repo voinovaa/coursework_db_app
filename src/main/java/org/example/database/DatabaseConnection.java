ff01102605064fe79dcbdba3ac087ffa591ce6db
Ekaterina <vkatrin1007@vk.com>
2026-06-24 19:07:30 +0700

    fixing

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -9,8 +9,7 @@ public class DatabaseConnection {
     private static DatabaseConnection instance;
     private Connection connection;
 
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/auto_parts_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/auto_parts_db";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
